e4886f9fee45330a0b570b6550767f4839a5a8e4
kandadave <kandachumo@gmail.com>
2025-06-07 04:51:26 +0300

    works with functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driverList=['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return driverList.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(driverList=['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return driverList.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, 
+    returnLastTwoDrivers 
+]
+
+function createFareMultiplier(number){
+    return function(fare){
+        return fare * number;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driverList=['Antonia', 'Nuru', 'Amari', 'Mo'], argument){
+    if (argument==returnFirstTwoDrivers){
+        return returnFirstTwoDrivers();
+    }
+    if (argument==returnLastTwoDrivers){
+        return returnLastTwoDrivers();
+    }
+}
